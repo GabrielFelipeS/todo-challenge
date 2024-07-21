4ece60180b1696f74c4ebfa2466f2c91a5e026c9
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-07-21 16:57:14 -0300

    feat: add unauthenticaded Route::group for the apis

--- Laravel-API/routes/api.php
+++ Laravel-API/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// with sanctum auth
+//Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
+//    Route::apiResource('users', UserController::class);
+//    Route::apiResource('tasks', TaskController::class);
+//});
+
+// without auth
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('tasks', TaskController::class);
+});
