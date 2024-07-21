efb66221f99fad3b149d0d8fcd32b36d30721bed
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-07-21 15:47:52 -0300

    feat: add Task model and extras

---
+++ Laravel-API/database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->count(50)
+            ->hasTasks(2, [
+                'status' => 'in_progress',
+            ])
+            ->create();
+
+        User::factory()
+            ->count(12)
+            ->hasTasks(4, [
+                'status' => 'completed',
+            ])
+            ->create();
+
+        User::factory()
+            ->count(20)
+            ->create();
+    }
+}
