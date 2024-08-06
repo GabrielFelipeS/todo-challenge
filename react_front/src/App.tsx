e599fa8d5de0c52fe9b1e2a2575133f33c8758ef
Gabriel Felipe <belebraz16@gmail.com>
2024-08-06 13:57:03 -0300

    chore: Atualização de cores do tailwind

--- react_front/src/App.tsx
+++ react_front/src/App.tsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    function handleClick() {
+        if (localStorage.theme === "dark" || !("theme" in localStorage)) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+
+        if (localStorage.theme === "dark") {
+            localStorage.theme = "light";
+        } else {
+            localStorage.theme = "dark";
+        }
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+
+          <div className={"text-ligth-primary dark:text-dark-primary dark:text-primary"}>
+              Hello World
+          </div>
+
+          <div onClick={handleClick} className={"text-ligth-primary dark:text-dark-primary dark:text-primary"}>
+              Change Mode (Dark/Light)
+          </div>
+
+          <div className={"flex gap-1"}>
+              <div className={"w-6 h-6 bg-ligth-primary dark:bg-dark-primary"}></div>
+              <div className={"w-6 h-6 bg-ligth-secondary dark:bg-dark-secondary"}></div>
+              <div className={"w-6 h-6 bg-ligth-tertiary dark:bg-dark-tertiary"}></div>
+              <div className={"w-6 h-6 bg-ligth-accent dark:bg-dark-accent"}></div>
+              <div className={"w-6 h-6 bg-ligth-container dark:bg-dark-container"}></div>
+              <div className={"w-6 h-6 bg-ligth-detail dark:bg-dark-detail"}></div>
+              <div className={"w-6 h-6 bg-ligth-border dark:bg-dark-border"}></div>
+              <div className={"w-6 h-6 bg-ligth-highlight dark:bg-dark-highlight"}></div>
+              <div className={"w-6 h-6 bg-ligth-screen dark:bg-dark-screen"}></div>
+              <div className={"w-6 h-6 bg-ligth-selected dark:bg-dark-selected"}></div>
+          </div>
+
+      </>
   )
 }
 
--- react_front/tailwind.config.js
+++ react_front/tailwind.config.js
@@ -1,9 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ligth: {
+          // Cores para texto
+          primary: '#000000',
+          secondary: '#BDC2CF',
+          tertiary: '#AEAEAE',
+          highlight: '#FD5553',
+          accent: '#51B156',
+
+          // Cor de fundo
+          screen: '#F7F7FA',
+
+          // Cor de fundo dos containers
+          container: '#FFFFFF',
+          selected: '#F5F5F5',
+          detail: '#FBFBFB',
+
+          // Bordas
+          border: '#51B156',
+        },
+        dark: {
+          // Cores para texto
+          primary: '#FFFFFF',
+          secondary: '#BDC2CF',
+          tertiary: '#AEAEAE',
+          highlight: '#02AAAC',
+          accent: '#02AAAC',
+
+          // Cor de fundo
+          screen: '#0F0F0F',
+
+          // Cor de fundo dos containers
+          container: '#090809',
+          selected: '#0E0E0E',
+
+          detail: '#121012',
+          // Bordas
+          border: '#474236',
+        }
+      }
+    },
   },
   plugins: [],
 }
 
+// primary: '#ff4500',
+//     secondary: '#9ca3af',
+//     background: '#1f2937',
+//     text: '#f9fafb',
