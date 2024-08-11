6963529c990f883d094c9fa3d5270e92c0dcb35b
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-08-11 16:24:08 -0300

    fix: All columns are returned on task seeding

--- Laravel-API/database/factories/TaskFactory.php
+++ Laravel-API/database/factories/TaskFactory.php
@@ -16,17 +16,24 @@ class TaskFactory extends Factory
      */
     public function definition(): array
     {
-        $status = $this->faker->randomElement(['pending', 'canceled']);
-        $assigned_at = $this->faker->dateTimeBetween('-2 years', '+2 months');
-        $completed_at = ($status == 'pending') ?
+        $status = $this->faker->randomElement(['pending', 'in_progress', 'completed', 'canceled']);
+
+        $created_updated_at = $this->faker->dateTimeBetween('-2 years', 'now');
+        $task_date = $this->faker->dateTimeBetween($created_updated_at, '+50 days');
+        $assigned_at = $this->faker->dateTimeBetween($created_updated_at, 'now');
+
+        $completed_at = ($status == 'completed') ?
             $this->faker->dateTimeBetween($assigned_at, '+2 months') : null;
+
         return [
             'title' => $this->faker->words(3, true),
             'description' => $this->faker->paragraph(),
             'status' => $status,
-            'due_date' => $this->faker->dateTime(),
+            'task_date' => $task_date,
             'assigned_at' => $assigned_at,
             'completed_at' => $completed_at,
+            'created_at' => $created_updated_at,
+            'updated_at' => $created_updated_at,
         ];
     }
 }
