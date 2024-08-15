c084247a12cd41ee11251c8a7a83ddc61c1e5e5e
Gabriel Felipe <belebraz16@gmail.com>
2024-08-15 18:23:25 -0300

    cleanup: Comments

--- react_front/src/components/MainComponent/ContainerTaskInfo/TaskData.tsx
+++ react_front/src/components/MainComponent/ContainerTaskInfo/TaskData.tsx
@@ -17,7 +17,6 @@ const Container = styled.div.attrs({
     className: "flex max-sm:flex-row justify-around items-center mt-3 h-[93px] " +
         "w-full rounded-lg bg-ligth-container dark:bg-dark-container " +
         "max-xs:flex-col max-xs:h-[160px] "
-       // "max-xs:grid max-xs:grid-rows-2 max-xs:grid-cols-2 max-xs:h-[120px] gap-4 "
 })``;
 
 const TaskAttribute = styled.div.attrs({
@@ -32,7 +31,6 @@ const Span = styled.span.attrs({
     className: "text-ligth-highlight dark:text-dark-highlight md:text-3xl"
 })``;
 
-
 export function TaskData({hours, minutes, pendingTasks, filteredTasks}: taskDataProps) {
     const containsHours = hours != 0;
     const concluded_tasks = filteredTasks.filter(COMPLETED_TASKS_FILTER).length
@@ -40,7 +38,6 @@ export function TaskData({hours, minutes, pendingTasks, filteredTasks}: taskData
 
     return(
         <Container>
-
             <TaskAttribute className="col-span-2">
                 <TaskInfo>
                     {containsHours ? <> <Span>{hours}</Span>h </> : ''}
